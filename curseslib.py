aab3651363642c5a2110408ca327d6df667e8732
LindaJeanne <lindajeanne73@gmail.com>
2015-01-02 13:51:43 -0500

    objectified curseslib.py

--- curseslib.py
+++ curseslib.py
@@ -1,118 +1,102 @@
 import curses
+import numpy as np
 
 
-_scr = None
-_regions = {}
+# _scr = None
+# _regions = {}
 
 
-# color pairs
-COLOR_WHITE_ON_BLACK = 1
-COLOR_BLUE_ON_BLACK = 2
-COLOR_GREEN_ON_BLACK = 3
-COLOR_RED_ON_BLACK = 4
-COLOR_YELLOW_ON_BLACK = 5
+class CursesSession(object):
+
+    # color pairs
+    COLOR_WHITE_ON_BLACK = 1
+    COLOR_BLUE_ON_BLACK = 2
+    COLOR_GREEN_ON_BLACK = 3
+    COLOR_RED_ON_BLACK = 4
+    COLOR_YELLOW_ON_BLACK = 5
+
+    def __init__(self):
+        self._scr = curses.initscr()
+        curses.start_color()
+        curses.noecho()
+        curses.cbreak()
+        curses.curs_set(0)
+        self._scr.keypad(True)
+
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 
 
-def setup():
-
-    global _scr
-
-    _scr = curses.initscr()
-    curses.start_color()
-    curses.noecho()
-    curses.cbreak()
-    _scr.keypad(True)
-
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-
-    global _regions
-    _regions['NONE'] = (0, 0, 0, 0)
+    def display_char(self, x, y, the_char, color=1):
+        self._scr.addch(y, x, the_char, curses.color_pair(color))
 
 
-def create_region(x, y, width, height, token):
+    def display_string(self, x, y, the_str):
+        if the_str:
+            self._scr.addstr(y, x, the_str)
 
-    _regions[token] = (x, y, width, height)
+    def wait_keypress(self):
+        return self._scr.getch()
 
+    def refresh(self):
+        self._scr.refresh()
 
-def end_curses():
-
-    global _scr
-
-    _scr.keypad(False)
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
-
-
-def display_char(x, y, char, color=1, region='NONE'):
-
-    global _scr
-    global _regions
-
-    _scr.addch(
-        y + _regions[region][1],
-        x + _regions[region][0],
-        char,
-        curses.color_pair(color))
-
-    refresh()
+    def end(self):
+        self._scr.keypad(False)
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
 
 
-def display_string(x, y, str, region='NONE'):
+class CursesWidget(object):
 
-    global _scr
-    global _regions
-
-    _scr.addstr(y + _regions[region][1], x + _regions[region][0], str)
-
-    refresh()
-
-
-def wait_keypress():
-    global _scr
-    return _scr.getch()
-
-#        _screenlist[screenToken].keypad(False)
-
-def end_curses():
-
-    global _scr
-    _scr.keypad(False)
-
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
-
-def refresh():
-    global _scr
-    _scr.refresh()
+    def __init__(self, csess, x, y, width, height):
+        self.session = csess
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
 
-#class CursesWidget(object):
-#
-#    def __init__(self, x, y, width, height):
-#        global _regions
-#        self.rect = (x, y, width, height)
-#        _regions[self] = self.rect
-#
-#
-#class CursesAsciiWidget(object):
-#
-#    def display_char(self, x, y, char, color=1):
-#
-#        global _regions
-#        display_char(x, y, char, color, _regions[self])
-#        curses.refresh()
-#
-#
-#class CursesMsgWidget(object):
-#
-#    def display_string(self, str):
-#
-#        global _regions
-#        display_string(1, 1, str, _regions[self])
-#        curses.refresh()
+    def display_string(self, x, y, the_str):
+        self.session.display_string(x + self.x, y + self.y, the_str)
+
+    def display_char(self, x, y, the_char, color=1):
+        self.session.display_char(
+            x + self.x,
+            y + self.y,
+            the_char,
+            color)
+
+    def clear_widget(self):
+        for i in self.x:
+            for j in self.y:
+                display_char(i, j, ' ')
+
+        self.session.refresh()
+
+
+class CursesMessageWidget(CursesWidget):
+
+    def display_message(self, the_str):
+        #self.clear_widget()
+        self.display_string(0, 0, the_str)
+        self.session.refresh()
+
+
+class CursesCharmapWidget(CursesWidget):
+
+    def draw_array(self, the_char_array):
+
+        for i, v in np.ndenumerate:
+            self.display_char(i[0], i[1], v)
+        self.session.refresh()
+
+    def draw_char(self, x, y, the_char, color=1):
+
+        self.display_char(x, y, the_char, color)
+        self.session.refresh()
+
--- display.py
+++ display.py
@@ -2,34 +2,65 @@ import curses
 import curseslib
 
 
-curseslib.create_region(1, 0, 40, 3, 'TOPMSG')
-curseslib.create_region(1, 3, 40, 40, 'MAPWIN')
-curseslib.create_region(1, 43, 40, 3, 'LOWMSG')
+_cur = None
 
+_topmsg = None
+_mapwin = None
+_lowmsg = None
 
-def display_top_message(msg):
-
-    curseslib.display_string(0, 1, msg, region='TOPMSG')
-
-
-def display_bottom_message(msg):
-
-    curseslib.display_string(0, 1, msg, region='LOWMSG')
+#curseslib.create_region(1, 0, 40, 3, 'TOPMSG')
+#curseslib.create_region(1, 3, 40, 40, 'MAPWIN')
+#curseslib.create_region(1, 43, 40, 3, 'LOWMSG')
 
 
 def setup():
 
-    curseslib.setup()
+    # curseslib.setup()
+    global _cur
+    global _topmsg
+    global _mapwin
+    global _lowmsg
+
+    _cur = curseslib.CursesSession()
+
+    _topmsg = curseslib.CursesMessageWidget(_cur, 1, 0, 40, 2)
+    _mapwin = curseslib.CursesCharmapWidget(_cur, 1, 3, 40, 40)
+    _lowmsg = curseslib.CursesMessageWidget(_cur, 1, 44, 40, 3)
+
+
+def display_top_message(msg):
+
+    # curseslib.display_string(0, 1, msg, region='TOPMSG')
+    global _topmsg
+    _topmsg.display_message(msg)
+
+
+def display_bottom_message(msg):
+
+    #curseslib.display_string(0, 1, msg, region='LOWMSG')
+    global _lowmsg
+    _lowmsg.display_message(msg)
 
 
 def display_map_char(x, y, char, color=1):
 
-    curseslib.display_char(x, y, char, color, 'MAPWIN')
+    global _mapwin
+    _mapwin.draw_char(x, y, char, color)
+    _cur.refresh
+
+
+def display_char_array(the_array):
+    global _mapwin
+    _mapwin.draw_array(the_array)
 
 
 def end_curses():
-    curseslib.end_curses()
+    #curseslib.end_curses()
+    global _cur
+    _cur.end()
 
 
 def wait_char():
-    return curseslib.wait_keypress()
+    #return curseslib.wait_keypress()
+    global _cur
+    return _cur.wait_keypress()
